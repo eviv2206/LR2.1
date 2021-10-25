4cf59c1ebca73debd680834a7308a3ffbdd01044
eviv2206 <zhenya.saprin@mail.ru>
2021-10-25 16:16:23 +0300

    initial commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,69 +1,64 @@
 package com.company;
-
 import java.util.Scanner;
-
 public class Main {
 
     public static void main(String[] args) {
-        int counter;
         System.out.println("This program checks whether the point lies on the side of the polygon");
-        counter = inputData();
-        checkCounter(counter);
-    }
-
-    public static int inputData(){
         Scanner input = new Scanner(System.in);
-        int counter = 0;
         int numOfVer;
+        int[] firstVector = new int[2];
+        int[] secondVector = new int[2];
+        int[] vectorOfSide = new int[2];
+        int[] vectorOfPoint = new int[2];
+        double finderBundle;
+        double scalarProduct;
+        int counter = 0;
         int[] pointCoord = new int[2];
-        numOfVer = checkInputData("Enter the number of vertices of the polygon: ", input);
-        while (numOfVer < 3){
-            numOfVer = checkInputData("Number of vertices should be more than 2: ", input);
-        }
-        int[][] verCoordinates = new int[2][numOfVer];
         pointCoord[0] = checkInputData("Enter x - coordinate of point: ", input);
         pointCoord[1] = checkInputData("Enter y - coordinate of point: ", input);
+        numOfVer = checkInputData("Enter the number of vertices of the polygon: ", input);
+        while (numOfVer < 3) {
+            numOfVer = checkInputData("Number of vertices should be more than 2: ", input);
+        }
+        int n = numOfVer - 1;
+        int[][] verCoordinates = new int[2][numOfVer];
         for (int i = 0; i < numOfVer; i++){
             verCoordinates[0][i] = checkInputData("Enter x - coordinate of vertex: ", input);
             verCoordinates[1][i] = checkInputData("Enter y - coordinate of vertex: ", input);
         }
-        counter = ifPointOnVector(pointCoord, verCoordinates, numOfVer);
         input.close();
-        return counter;
-    }
-
-    public static int ifPointOnVector(int[] pointCoord, int[][] verCoordinates, int numOfVer) {
-        int counter = 0;
-        int[] vectorOfSide = new int[2];
-        int[] vectorOfPoint = new int[2];
-        int n = numOfVer - 1;
-        double finderBundle;
-        for (int i = 0; i < n; i++){
+        for (int i = 0; i < n; i++) {
+            firstVector[0] = verCoordinates[0][i] - pointCoord[0];
+            firstVector[1] = verCoordinates[1][i] - pointCoord[1];
+            secondVector[0] = verCoordinates[0][i+1] - pointCoord[0];
+            secondVector[1] = verCoordinates[1][i+1] - pointCoord[1];
             vectorOfSide[0] = verCoordinates[0][i] - verCoordinates[0][i+1];
             vectorOfSide[1] = verCoordinates[1][i] - verCoordinates[1][i+1];
             vectorOfPoint[0] = pointCoord[0] - verCoordinates[0][i];
             vectorOfPoint[1] = pointCoord[1] - verCoordinates[1][i];
             finderBundle = vectorOfSide[0] * vectorOfPoint[1] - vectorOfPoint[0] * vectorOfSide[1];
-            if (finderBundle == 0){
+            scalarProduct = firstVector[0] * secondVector[0] + firstVector[1] * secondVector[1];
+            if (scalarProduct < 1 && finderBundle == 0) {
                 counter++;
             }
         }
-        vectorOfSide[0] = verCoordinates[0][0] - verCoordinates[0][n];
-        vectorOfSide[1] = verCoordinates[1][0] - verCoordinates[1][n];
-        vectorOfPoint[0] = pointCoord[0] - verCoordinates[0][0];
-        vectorOfPoint[1] = pointCoord[1] - verCoordinates[1][0];
+        firstVector[0] = verCoordinates[0][n-1] - pointCoord[0];
+        firstVector[1] = verCoordinates[1][n-1] - pointCoord[1];
+        secondVector[0] = verCoordinates[0][n] - pointCoord[0];
+        secondVector[1] = verCoordinates[1][n] - pointCoord[1];
+        vectorOfSide[0] = verCoordinates[0][n] - verCoordinates[0][n-1];
+        vectorOfSide[1] = verCoordinates[1][n] - verCoordinates[1][n-1];
+        vectorOfPoint[0] = pointCoord[0] - verCoordinates[0][n];
+        vectorOfPoint[1] = pointCoord[1] - verCoordinates[1][n];
         finderBundle = vectorOfSide[0] * vectorOfPoint[1] - vectorOfPoint[0] * vectorOfSide[1];
-        if (finderBundle == 0){
+        scalarProduct = firstVector[0] * secondVector[0] + firstVector[1] * secondVector[1];
+        if (scalarProduct < 1 && finderBundle == 0){
             counter++;
         }
-        return counter;
-    }
-
-    public static void checkCounter(int counter){
         if (counter > 0){
             System.out.println("The point lies on the side of polygon");
-        } else{
-            System.out.println("The point doesn't lies on the side of polygon");
+        } else {
+            System.out.println("The point doesn't lie on the side of polygon");
         }
     }
     public static int checkInputData(String str, Scanner input) {
@@ -81,5 +76,4 @@ public class Main {
         } while (isIncorrect);
         return number;
     }
-
 }
